3526640209967f30efd96283ca4043ed2739420e
Native856 <46538183+Native856@users.noreply.github.com>
2021-11-25 14:15:30 +0100

    Update admin.py

--- BookProject/BookApp/admin.py
+++ BookProject/BookApp/admin.py
@@ -5,7 +5,7 @@ from .models import BookModels
 class AdminBookModels(admin.ModelAdmin):
     list_display = ('title', 'author', 'pub_date', 'nr_isbn_one', 'nr_isbn_two', 'page_co', 'img_link',
                     'pub_lang')
-    list_filter = ('pub_lang')
+    list_filter = ('pub_lang',)
     search_fields = ('title',)
     
 
